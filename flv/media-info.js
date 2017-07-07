fa807ec175e8d7c4bb5b9cbf8d69e80ffe99775c
gaolingxiao <gaolingxiao@360.cn>
2017-07-07 11:27:31 +0800

    初次提交

---
+++ flv/media-info.js
@@ -0,0 +1,73 @@
+/**
+ * reference flv.js
+ */
+/* eslint-disable */
+class MediaInfo {
+
+    constructor() {
+        this.mimeType = null;
+        this.duration = null;
+
+        this.hasAudio = null;
+        this.hasVideo = null;
+        this.audioCodec = null;
+        this.videoCodec = null;
+        this.audioDataRate = null;
+        this.videoDataRate = null;
+
+        this.audioSampleRate = null;
+        this.audioChannelCount = null;
+
+        this.width = null;
+        this.height = null;
+        this.fps = null;
+        this.profile = null;
+        this.level = null;
+        this.chromaFormat = null;
+        this.sarNum = null;
+        this.sarDen = null;
+
+        this.metadata = null;
+        this.segments = null; // MediaInfo[]
+        this.segmentCount = null;
+        this.hasKeyframesIndex = null;
+        this.keyframesIndex = null;
+    }
+
+    isComplete() {
+        const audioInfoComplete = (this.hasAudio === false) ||
+            (this.hasAudio === true &&
+                this.audioCodec != null &&
+                this.audioSampleRate != null &&
+                this.audioChannelCount != null);
+
+        const videoInfoComplete = (this.hasVideo === false) ||
+            (this.hasVideo === true &&
+                this.videoCodec != null &&
+                this.width != null &&
+                this.height != null &&
+                this.fps != null &&
+                this.profile != null &&
+                this.level != null &&
+                this.chromaFormat != null &&
+                this.sarNum != null &&
+                this.sarDen != null);
+
+        // keyframesIndex may not be present
+        return this.mimeType != null &&
+            this.duration != null &&
+            this.metadata != null &&
+            this.hasKeyframesIndex != null &&
+            audioInfoComplete &&
+            videoInfoComplete;
+    }
+
+    isSeekable() {
+        return this.hasKeyframesIndex === true;
+    }
+
+
+
+}
+
+export default MediaInfo;
